721c57aef9e3226d08d4fbdd5c991680a557e082
tt2819ais-arch <tt2819ais@gmail.com>
2026-02-28 18:33:05 +0300

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,33 @@
+const CACHE = 'start-v2';
+const SHELL = ['/', '/index.html'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  const url = e.request.url;
+  if (url.includes('/tracks/') && (url.endsWith('.mp3') || url.endsWith('.mp4'))) {
+    e.respondWith(fetch(e.request));
+    return;
+  }
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).then(res => {
+      if (res.ok && e.request.method === 'GET') {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+      }
+      return res;
+    }))
+  );
+});
